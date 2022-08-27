91aa0a80b9d966366e496a9d21b291881ff3e259
wulovey <75718566+wulovey@users.noreply.github.com>
2022-08-27 12:44:24 +0800

    Update xiaoma.js

--- xiaoma.js
+++ xiaoma.js
@@ -8,14 +8,12 @@ QuantumultX:
 ^https:\/\/api\.xiaomapeilian\.com\/(ai-app-api\/v1\/book\/previewMusic|ai-app-api\/v1\/book\/musicList|ai-app-api\/v1\/pianoTrain\/getSimpleDate) url script-response-body https://raw.githubusercontent.com/wulovey/QX/main/xiaoma.js
 
 [mitm]
-hostname = api.xiaomapeilian.com
+hostname = dss.xiaoyezi.com
 
 ***************************
 
 **************************/
-var body = $response.body;
-var url = $request.url;
 
-	body=body.replace("\"isFree\":0","\"isFree\":1");
-	body=body.replace("\"hasFree\":0","\"hasFree\":1");
-$done({body});
+var body = $response.body
+    .replace("\"student_status\":\d", "\"student_status\":2")
+$done({ body });
